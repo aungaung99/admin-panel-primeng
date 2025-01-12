4e9be1de66d1d7a807bc881b2ab7333c0efe7024
Aung Naing Oo <aungaung20.ab@gmail.com>
2025-01-12 22:11:45 +0630

    complete topbar and sidebar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { AppLayout } from './layout/component/app.layout';
+import { HomeComponent } from './pages/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: AppLayout,
+    children: [
+      { path: '', component: HomeComponent },
+    ]
+  }
+];
