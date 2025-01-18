fb714f998e560d2ad56d8a47bc609e36ae094a18
Aung Naing Oo <aungaung20.ab@gmail.com>
2025-01-19 01:57:09 +0630

    update css

--- src/app/layout/component/app.sidebar.ts
+++ src/app/layout/component/app.sidebar.ts
@@ -2,13 +2,33 @@ import { Component, ElementRef } from '@angular/core';
 import { AppMenu } from './app.menu';
 
 @Component({
-    selector: 'app-sidebar',
-    standalone: true,
-    imports: [AppMenu],
-    template: ` <div class="layout-sidebar">
+  selector: 'app-sidebar',
+  standalone: true,
+  imports: [AppMenu],
+  styles: [
+    /* Add custom scrollbar styles here */
+    `::-webkit-scrollbar {
+        width: 6px;
+      }
+
+      ::-webkit-scrollbar-track {
+        background: var(--scroll-bg-color);
+        height: 16px;
+      }
+
+      ::-webkit-scrollbar-thumb {
+        background: var(--scroll-bar-color);
+        border-radius: 10px;
+      }
+
+      ::-webkit-scrollbar-thumb:hover {
+        background: #555;
+      }`
+  ],
+  template: ` <div class="layout-sidebar">
         <app-menu></app-menu>
     </div>`
 })
 export class AppSidebar {
-    constructor(public el: ElementRef) {}
+  constructor(public el: ElementRef) { }
 }
