a8414424c3e7ef18f7ac965e1a9d1c47433e780e
Aung Naing Oo <aungaung20.ab@gmail.com>
2025-01-19 02:38:13 +0630

    update font-size

--- src/app/layout/component/app.topbar.ts
+++ src/app/layout/component/app.topbar.ts
@@ -3,14 +3,13 @@ import { MenuItem } from 'primeng/api';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { StyleClassModule } from 'primeng/styleclass';
-import { AppConfigurator } from './app.configurator';
 import { LayoutService } from '../service/layout.service';
 import { BreadCrumbComponent } from "../../shared/bread-crumb/bread-crumb.component";
 
 @Component({
   selector: 'app-topbar',
   standalone: true,
-  imports: [RouterModule, CommonModule, StyleClassModule, AppConfigurator, BreadCrumbComponent],
+  imports: [RouterModule, CommonModule, StyleClassModule, BreadCrumbComponent],
   template: ` <div class="layout-topbar">
         <div class="layout-topbar-logo-container">
             <button class="layout-menu-button layout-topbar-action" (click)="layoutService.onMenuToggle()">
